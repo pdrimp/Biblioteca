d65f7134288ef9dc0d547c9ebbf4cab7fda32445
damian <damian@ucol.mx>
2023-05-25 10:17:53 -0600

    Cambios en editoriales

--- Biblioteca/Shared/Modelos/Editorial.cs
+++ Biblioteca/Shared/Modelos/Editorial.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,7 @@ namespace Biblioteca.Shared.Modelos
     public class Editorial
     {
         public int Id { get; set; }
+        [Required(ErrorMessage ="El nombre es obligatorio")]
         public string? Nombre { get; set; }
 
         public virtual ICollection<Libro>? Libros { get; set; }
